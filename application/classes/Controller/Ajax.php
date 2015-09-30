ef47f56220afd5b6fead812cd033af7cbcd6586e
Liubomyr Matveychuk <liubomyr.matveychuk@gmail.com>
2015-09-30 14:49:26 +0300

    basic dev

---
+++ application/classes/Controller/Ajax.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Ajax extends Controller {
+
+	public function action_request()
+	{
+		if(is_array($_POST)) {
+			foreach($_POST as $k => $v){
+                switch($k){
+                    case 'login-request':
+                        $info = Model::factory('Users')->check_user($v);
+                        if(!empty($info)){
+                            Session::instance()->set('user',$info[0]);
+                            echo true;
+                        }
+                        break;
+                }
+            };
+		}
+	}
+
+
+}
